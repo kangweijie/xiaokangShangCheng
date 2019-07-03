b81c3542134be8538ceb865d07b0b8b6bc0fa352
kangweijie <839354343@qq.com>
2019-07-03 19:19:01 +0800

    tijiao

--- pages/home/home.js
+++ pages/home/home.js
@@ -9,7 +9,22 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
+
     goodsRecommend: [], // 推荐商品
+    inputShowed: false,
+    inputVal: "",
+    noticeList: ['爆品上新通知','物流派送信息通知'],
+    indicatorDots:false,
+    autoplay:true,
+    interval:3000,
+    duration:1000,
+    circular:true,
+    bannerList:[
+      { picUrl: "https://cdn.it120.cc/apifactory/2019/03/18/7de640d0a0c0c3f525df0e6b65abb1f0.png"},
+      {picUrl: "https://cdn.it120.cc/apifactory/2019/03/18/4714a6caa0be3c4153b747562a4337b6.png"},
+      {picUrl: "https://cdn.it120.cc/apifactory/2019/03/18/c7c8e67ca1b6ff23efa13b5d7516a9d6.png"}
+    ]
+
   },
   onLoad: function () {
     /* 请求接口 */
@@ -19,6 +34,27 @@ Page({
           goodsRecommend: res.data 
         });
       }
+    });
+  },
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
     });
   }
 })
